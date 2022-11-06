4fe30239fd6de55a94d5f152f5df2ce555d844f3
morot2 <morot3060@gmail.com>
2022-11-06 15:11:43 +0900

    time api

--- src/main/kotlin/com/example/demo/HelloController.kt
+++ src/main/kotlin/com/example/demo/HelloController.kt
@@ -2,6 +2,7 @@ package com.example.demo
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 
 @RestController
 class HelloController {
@@ -14,5 +15,10 @@ class HelloController {
     @GetMapping("/hello")
     fun hello(): String {
         return "hello world"
+    }
+
+    @GetMapping("/time")
+    fun time(): String {
+        return LocalDateTime.now().toString()
     }
 }
